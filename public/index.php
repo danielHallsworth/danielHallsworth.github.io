05bf7ce9dd560ab496c32b919a285e32851b193a
danielHallsworth <daniel.hallsworth97@gmail.com>
2021-04-10 09:00:34 +0100

    formatting and adding bootstrap

---
+++ public/index.php
@@ -0,0 +1,96 @@
+<!DOCTYPE HTML>
+<html>
+	<head>
+		<title>Daniel Hallsworth Portfolio</title>
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
+		<link rel="stylesheet" href="../assets/css/main.css" />
+		<noscript><link rel="stylesheet" href="../assets/css/noscript.css" /></noscript>
+	</head>
+	<body class="is-preload">
+
+		<!-- Wrapper -->
+			<div id="wrapper" class="fade-in">
+
+				<!-- Intro -->
+					<div id="intro">
+						<h1>Daniel Hallsworth<br />
+						Portfolio</h1>
+						<p>Full stack <u title="Web based software development that runs on an Oracle Database">Oracle APEX developer</u> with 18 months professional experiance</p>
+						<ul class="actions">
+							<li><a href="#projects" class="button icon solid solo fa-arrow-down scrolly">Continue</a></li>
+						</ul>
+					</div>
+
+				<!-- Header -->
+					<header id="header">
+						<a href="index.php" class="logo">Projects</a>
+					</header>
+
+				<!-- Nav -->
+					<nav id="nav">
+						<ul class="links">
+							<li class="active"><a href="index.php">My Projects</a></li>
+						</ul>
+						<ul class="icons">
+							<li><a href="https://www.linkedin.com/in/danielHallsworth/" class="icon brands fa-linkedin"><span class="label">Linked in</span></a></li>
+							<li><a href="https://github.com/danielHallsworth" class="icon brands fa-github"><span class="label">GitHub</span></a></li>
+						</ul>
+					</nav>
+
+				<!-- Main -->
+					<div id="main">
+
+						<!-- Featured Post -->
+							<article class="post featured">
+								<header class="major">
+									
+									<h2 id="projects"><a href="#">Roulette<br />
+									</a></h2>
+									<p> A roulette game coded in JavaScript, the main purpose of this app was to practice DOM manipulation in JavaScript </p>
+								</header>
+								<a  href="#" class="image main"><img src="../images/rouletteImg.jpg" alt="" /></a>
+								<ul class="actions special">
+									<li><a href="https://github.com/danielHallsworth/danielHallsworth.github.io" class="button large">Code</a></li>
+									<li><a href="rouletteIndex.html" class="button large">View</a></li>
+								</ul>
+							</article>
+
+						
+					</div>
+
+				<!-- Footer -->
+					<footer id="footer">
+						<section class="split contact">
+							<section>
+								<h3>Email</h3>
+								<p>daniel.hallsworth97@gmail.com</p>
+							</section>
+							<section>
+								<h3>Social</h3>
+								<ul class="icons alt">
+									<li><a href="https://linkedin.com/in/danielHallsworth" class="icon brands alt fa-linkedin"><span class="label">LinkedIn</span></a></li>
+									<li><a href="https://github.com/danielHallsworth" class="icon brands alt fa-github"><span class="label">GitHub</span></a></li>
+								</ul>
+							</section>
+						</section>
+					</footer>
+
+				<!-- Copyright -->
+					<div id="copyright">
+						<ul><li>Daniel Hallsworth</li></ul>
+					</div>
+
+			</div>
+
+		<!-- Scripts -->
+			<script src="../assets/js/jquery.min.js"></script>
+			<script src="../assets/js/jquery.scrollex.min.js"></script>
+			<script src="../assets/js/jquery.scrolly.min.js"></script>
+			<script src="../assets/js/browser.min.js"></script>
+			<script src="../assets/js/breakpoints.min.js"></script>
+			<script src="../assets/js/util.js"></script>
+			<script src="../assets/js/main.js"></script>
+
+	</body>
+</html>
